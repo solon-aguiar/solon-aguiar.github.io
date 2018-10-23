13d7b9e9249f3ccd0ca01d928bfd4d9ed98bd56d
Solon Aguiar <solon@spotify.com>
2018-10-23 11:35:42 -0400

    Add serviceWorker to cache pages

---
+++ _site/serviceWorker.js
@@ -0,0 +1,57 @@
+var CACHE_NAME = 'v1';
+var URLS_TO_CACHE = [
+  '/',
+  '/about',
+  '/assets/main.css',
+  '/assets/images/aboutMe.gif'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(function(response) {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return reponse;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(function(cache){
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
