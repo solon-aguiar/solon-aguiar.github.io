8b048640427f129d304bd8a1251188f871261f0b
Solon Aguiar <solon.liam.aguiar@gmail.com>
2020-10-09 17:32:50 -0700

    fix site

--- _site/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
-    }, function(error) {
-      console.error('failed to register serviceWorker', error);
-    });
-  });
-}
